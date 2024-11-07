7eac203d7352983b0b3fce9dbe7ed7a63ffced63
Rushi Behara <rushibehara4@gmail.com>
2024-11-07 14:02:48 +0530

    change the path

--- src/Components/RegisterPage.jsx
+++ src/Components/RegisterPage.jsx
@@ -96,7 +96,7 @@ const Register = () => {
         throw new Error(data.message || "Network response was not ok");
 
       toast.success("Form submitted successfully!");
-      navigate("/"); // This should work to redirect after submission
+      navigate("/userDetails"); // This should work to redirect after submission
     } catch (error) {
       console.error("Error:", error);
       toast.error(error.message || "An error occurred, please try again later");
